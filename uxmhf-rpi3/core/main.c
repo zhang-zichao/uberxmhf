cca87487460350d0b5940f204dfae6758802da66
zzc <zichao.zzc@gmail.com>
2023-10-20 00:27:02 -0400

    test timer

--- uxmhf-rpi3/core/main.c
+++ uxmhf-rpi3/core/main.c
@@ -556,7 +556,8 @@ void main(u32 r0, u32 id, struct atag *at, u32 cpuid){
 		uapp_pvdriver_uart_initialize_uapp(cpuid);
 	#endif
 
-
+	uapp_watchdog_initialize(0);
+	
 	//////
 
 	// boot secondary cores
