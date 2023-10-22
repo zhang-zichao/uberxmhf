18b62decf5e9db4df59a73a0178eb24db7f9acbc
zzc <zichao.zzc@gmail.com>
2023-10-22 01:00:55 -0400

    debug

--- uxmhf-rpi3/uapps/uapp-watchdog/uapp-watchdog.c
+++ uxmhf-rpi3/uapps/uapp-watchdog/uapp-watchdog.c
@@ -143,9 +143,9 @@ void uapp_watchdog_timerhandler(void){
 
 
 void uapp_watchdog_initialize(u32 cpuid){
-	return;
 	if(cpuid == 0){
 		hypvtable_setentry(cpuid, 7, (u32)&uapp_watchdog_fiq_handler);
+		return;
 		uapp_watchdog_timer_initialize(cpuid);
 
 	}
